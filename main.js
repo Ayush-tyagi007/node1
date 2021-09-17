c6815ac0c59e079b3b075b3b678bf100f44e2a24
Ayush-tyagi007 <ayushtyagi6395@gmail.com>
2021-09-17 18:17:47 +0530

    inserted 5 users

--- main.js
+++ main.js
@@ -1,43 +1,76 @@
-const mongoose=require('mongoose');
-const {User,UserProfile}=require("./model");
-const md5 = require('md5');
-mongoose.connect('mongodb+srv://ayush:tyagi.apj@cluster0.oix8x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-	{
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-		
-	}).then(val=>{console.log('database is connected' )})
-    .catch(err=>{console.log(err)
-    })
-
-    
-    const userData = 
-        {
-        
-        firstName: "ayush",
-        lastName: "tyagi",
-        email: "ayush@123",
-        password:md5(12345)
-        }
-    
-    
-User.create(userData)
-.then(user=>{
-  const userProfileData={
-      mobile_no:1234567812,
-      dob:new Date('2018-06-29'),
-      
-      user_id:user.user_id
-
-
+const mongoose = require("mongoose");
+const { User, UserProfile } = require("./model");
+const md5 = require("md5");
+mongoose
+  .connect(
+    "mongodb+srv://ayush:tyagi.apj@cluster0.oix8x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     }
-    UserProfile.create(userProfileData)
-    .then(value=>{console.log(value)})
-    .catch(er=>{console.log(er)})
+  )
 
-    
-}).catch(error=>{
-    console.log(error)
-})
+  .then((val) => {
+    console.log("database is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+const userData = [
+  {
+    firstName: "ayush",
+    lastName: "tyagi",
+    email: "ayush@123",
+    password: md5(12345),
+  },
 
+  {
+    firstName: "ayush",
+    lastName: "tyagi",
+    email: "ayush@123",
+    password: md5(12345),
+  },
+  {
+    firstName: "ayush",
+    lastName: "tyagi",
+    email: "ayush@123",
+    password: md5(12345),
+  },
+  {
+    firstName: "ayush",
+    lastName: "tyagi",
+    email: "ayush@123",
+    password: md5(12345),
+  },
+];
 
+const userProfileData = [
+  {
+    dob: new Date("2018-06-29"),
+    mobile_no: 12324335,
+  },
+  {
+    dob: new Date("2018-06-29"),
+    mobile_no: 234345245,
+  },
+  {
+    dob: new Date("2018-06-29"),
+    mobile_no: 23454,
+  },
+  {
+    dob: new Date("2018-06-29"),
+    mobile_no: 2134324,
+  },
+  {
+    dob: new Date("2018-06-29"),
+    mobile_no: 23434877,
+  },
+];
+
+const getdocument = async (userData, userProfileData) => {
+  a = User.insertMany(userData);
+  b = UserProfile.insertMany(userProfileData);
+  //   const result = await User.find();
+  //   console.log(result[1]._id);
+};
+getdocument(userData, userProfileData);
--- model.js
+++ model.js
@@ -1,19 +1,18 @@
-const mongoose = require('mongoose');
-var md5 = require('md5');
-
+const mongoose = require("mongoose");
+var md5 = require("md5");
 
 const userSchema = new mongoose.Schema({
-    firstName: String,
-    lastName: String,
-    email: String,
-    password:String
+  firstName: String,
+  lastName: String,
+  email: String,
+  password: String,
 });
 
 const userProfileSchema = new mongoose.Schema({
-    user_id:mongoose.Schema.Types.ObjectId ,
-    dob:Date,
-    mobile_no:Number
+//   user_id: mongoose.Schema.Types.ObjectId,
+  dob: Date,
+  mobile_no: Number,
 });
-const User= mongoose.model('User', userSchema);
-const UserProfile= mongoose.model('UserProfile', userProfileSchema);
-module.exports={User,UserProfile}
+const User = mongoose.model("User", userSchema);
+const UserProfile = mongoose.model("UserProfile", userProfileSchema);
+module.exports = { User, UserProfile };
