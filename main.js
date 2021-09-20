aeca2da0d6032e7745796958375c1e986e035601
Ayush-tyagi007 <ayushtyagi6395@gmail.com>
2021-09-20 11:25:31 +0530

    task 1 completed of node js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const { User, UserProfile } = require("./model");
 const md5 = require("md5");
-mongoose
+db = mongoose
   .connect(
     "mongodb+srv://ayush:tyagi.apj@cluster0.oix8x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     {
@@ -21,56 +21,72 @@ const userData = [
     firstName: "ayush",
     lastName: "tyagi",
     email: "ayush@123",
-    password: md5(12345),
+    password: md5(1234521),
+    gender: "male",
   },
 
   {
-    firstName: "ayush",
+    firstName: "ankur",
     lastName: "tyagi",
-    email: "ayush@123",
-    password: md5(12345),
+    email: "ankur@123",
+    password: md5(1234578),
+    gender: "male",
   },
   {
-    firstName: "ayush",
+    firstName: "atul",
     lastName: "tyagi",
-    email: "ayush@123",
-    password: md5(12345),
+    email: "atul@123",
+    password: md5(1234590),
+    gender: "male",
   },
   {
-    firstName: "ayush",
+    firstName: "abhi",
     lastName: "tyagi",
-    email: "ayush@123",
+    email: "abhi@123",
     password: md5(12345),
+    gender: "male",
+  },
+  {
+    firstName: "akshay",
+    lastName: "tyagi",
+    email: "akshay@123",
+    password: md5(1234545),
+    gender: "male",
   },
 ];
 
-const userProfileData = [
-  {
-    dob: new Date("2018-06-29"),
-    mobile_no: 12324335,
-  },
-  {
-    dob: new Date("2018-06-29"),
-    mobile_no: 234345245,
-  },
-  {
-    dob: new Date("2018-06-29"),
-    mobile_no: 23454,
-  },
-  {
-    dob: new Date("2018-06-29"),
-    mobile_no: 2134324,
-  },
-  {
-    dob: new Date("2018-06-29"),
-    mobile_no: 23434877,
-  },
-];
+async function myfunction(userData) {
+  const a = await User.create(userData);
+  console.log(a[0]._id);
 
-const getdocument = async (userData, userProfileData) => {
-  a = User.insertMany(userData);
-  b = UserProfile.insertMany(userProfileData);
-  //   const result = await User.find();
-  //   console.log(result[1]._id);
-};
-getdocument(userData, userProfileData);
+  const userProfileData = [
+    {
+      dob: new Date("2018-06-29"),
+      mobile_no: 12324335,
+      user_id: a[0]._id,
+    },
+    {
+      dob: new Date("2009-06-20"),
+      mobile_no: 234345245,
+      user_id: a[1]._id,
+    },
+    {
+      dob: new Date("1970-06-22"),
+      mobile_no: 23454,
+      user_id: a[2]._id,
+    },
+    {
+      dob: new Date("1995-06-27"),
+      mobile_no: 2134324,
+      user_id: a[3]._id,
+    },
+    {
+      dob: new Date("2001-06-24"),
+      mobile_no: 23434877,
+      user_id: a[4]._id,
+    },
+  ];
+  const userProfile = UserProfile.create(userProfileData);
+
+}
+myfunction(userData);
--- model.js
+++ model.js
@@ -6,13 +6,15 @@ const userSchema = new mongoose.Schema({
   lastName: String,
   email: String,
   password: String,
+  gender:String
 });
 
 const userProfileSchema = new mongoose.Schema({
-//   user_id: mongoose.Schema.Types.ObjectId,
+  user_id: mongoose.Schema.Types.ObjectId,
   dob: Date,
   mobile_no: Number,
 });
+
 const User = mongoose.model("User", userSchema);
 const UserProfile = mongoose.model("UserProfile", userProfileSchema);
 module.exports = { User, UserProfile };
